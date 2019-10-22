c0a921c4c7135997bd78008120976882cfd88d17
Ana Claudia Lima de Almeida <almeidannynha@gmail.com>
2019-10-22 14:08:08 -0300

    [BUG] Resolvendo problemas na conexão com BD - Ainda faltam

--- src/main/java/com/uem/sga/controller/ItemTreinoController.java
+++ src/main/java/com/uem/sga/controller/ItemTreinoController.java
@@ -1,4 +1,7 @@
 package com.uem.sga.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ItemTreinoController {
 }
--- src/main/java/com/uem/sga/repository/TreinoRepository.java
+++ src/main/java/com/uem/sga/repository/TreinoRepository.java
@@ -1,4 +1,13 @@
 package com.uem.sga.repository;
 
-public class TreinoRepository {
+import model.Aluno;
+import model.Treino;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TreinoRepository extends JpaRepository<Treino,Long> {
+
+    Optional<Treino> findById(Long id);
+    Optional<Treino> findByAluno(Aluno aluno);
 }
