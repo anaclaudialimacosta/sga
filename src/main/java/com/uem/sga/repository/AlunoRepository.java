3f1b31781188d6383ba69729f8941b774e7a00e8
Diogo <diogomsales314@gmail.com>
2019-10-22 23:34:52 -0300

    adequação de aluno e professor  repositories

---
+++ src/main/java/com/uem/sga/repository/AlunoRepository.java
@@ -0,0 +1,11 @@
+package com.uem.sga.repository;
+
+import com.uem.sga.model.Aluno;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunoRepository extends CrudRepository<Aluno, Long> {
+
+    Aluno findAlunoById(Long idAluno);
+}
