5163222b59012eb33e03c7cca06a430661a1e27c
anamaganha <anapaulamaganha@gmail.com>
2019-10-22 22:39:54 -0300

    [DEV] Cadastro Usuario

--- src/main/java/com/uem/sga/service/ValidacaoUsuarioService.java
+++ src/main/java/com/uem/sga/service/ValidacaoUsuarioService.java
@@ -1,11 +1,24 @@
 package com.uem.sga.service;
 
+import com.uem.sga.model.TipoUsuario;
+import com.uem.sga.model.Usuario;
+import com.uem.sga.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ValidacaoUsuarioService {
 
-    public boolean validarUsuario(){
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
+    public boolean validarUsuario(String username, String senha){
+
+        Optional<Usuario> usuario = usuarioRepository.findByUsername(username);
+        if(usuario.isPresent()){
+            usuario.get().equals()
+        }
     }
 }
